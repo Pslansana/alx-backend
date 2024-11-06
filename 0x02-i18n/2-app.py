1bc40c981d52b8594367fa93c313d5d2ea338013
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-11-06 02:21:27 +0000

    Basic Babel setup

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
-""" This is a module run the index page on task 0 """
+"""
+Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
 
 
-from flask import Flask, render_template, request
-import flask_babel
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-babel = flask_babel.Babel(app)
-
-
-class Config:
-    """ This is a Config class that handle the babel configuration """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """ This is a function that return the locale via request """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('1-index.html')
 
 
-@app.route('/')
-def index():
-    """ This is a function that render index page """
-    return render_template('2-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
